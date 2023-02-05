945215f4f557325e09dbf9d48fe20d68802c27e6
nisali <nisalikularatne@gmail.com>
2023-02-05 14:34:42 -0500

    merge workflows

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,7 +1,7 @@
 # Define ECR repo to store database images
 resource "aws_ecr_repository" "database" {
-  name = "docker-${var.env}-containers-database-repo"
-
+  name         = "docker-${var.env}-containers-database-repo"
+  force_delete = true
   tags = {
     Name = "docker-${var.env}-containers-database-repo"
   }
@@ -9,8 +9,8 @@ resource "aws_ecr_repository" "database" {
 
 # Define ECR repo to store application images
 resource "aws_ecr_repository" "application" {
-  name = "docker-${var.env}-containers-application-repo"
-
+  name         = "docker-${var.env}-containers-application-repo"
+  force_delete = true
   tags = {
     Name = "docker-${var.env}-containers-application-repo"
   }
